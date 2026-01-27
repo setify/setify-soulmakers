6bce4a622df53e9933504e7e2a26a36bce3968fa
Piwi <mail@setify.de>
2026-01-27 23:29:31 +0100

    Aktualisiere Versionsnummer auf 1.0.1 und verbessere den Code-Stil

--- soulmakers.php
+++ soulmakers.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * Plugin Name:       Soulmakers
  * Plugin URI:        https://soulmakers.de
  * Description:       Soulmakers WordPress Plugin - Erweiterte Funktionalitäten für die Soulmakers Website.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Setify
  * Author URI:        https://setify.io
  * License:           GPL-2.0+
@@ -15,34 +16,34 @@
  */
 
 // Direkten Zugriff verhindern
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
 /**
  * Plugin-Konstanten definieren
  */
-define( 'SOULMAKERS_VERSION', '1.0.0' );
-define( 'SOULMAKERS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'SOULMAKERS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'SOULMAKERS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-define( 'SOULMAKERS_ASSETS_URL', SOULMAKERS_PLUGIN_URL . 'assets/' );
+define('SOULMAKERS_VERSION', '1.0.0');
+define('SOULMAKERS_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('SOULMAKERS_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('SOULMAKERS_PLUGIN_BASENAME', plugin_basename(__FILE__));
+define('SOULMAKERS_ASSETS_URL', SOULMAKERS_PLUGIN_URL . 'assets/');
 
 /**
  * Autoloader für Plugin-Klassen
  */
-spl_autoload_register( function( $class ) {
+spl_autoload_register(function ($class) {
     $prefix = 'Soulmakers_';
 
-    if ( strpos( $class, $prefix ) !== 0 ) {
+    if (strpos($class, $prefix) !== 0) {
         return;
     }
 
-    $class_name = str_replace( $prefix, '', $class );
-    $class_name = strtolower( str_replace( '_', '-', $class_name ) );
+    $class_name = str_replace($prefix, '', $class);
+    $class_name = strtolower(str_replace('_', '-', $class_name));
     $file = SOULMAKERS_PLUGIN_DIR . 'includes/class-soulmakers-' . $class_name . '.php';
 
-    if ( file_exists( $file ) ) {
+    if (file_exists($file)) {
         require_once $file;
     }
 });
@@ -57,7 +58,8 @@ require_once SOULMAKERS_PLUGIN_DIR . 'includes/class-soulmakers.php';
  *
  * @return Soulmakers
  */
-function soulmakers() {
+function soulmakers()
+{
     return Soulmakers::instance();
 }
 
